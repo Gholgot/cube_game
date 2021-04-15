241e32d848df5b6bcc1602095d55d0978acef4fe
unknown <tjacomelli@gmail.com>
2021-04-15 21:30:58 +0200

    Saving commit

--- cube_game-lib/src/player.rs
+++ cube_game-lib/src/player.rs
@@ -11,7 +11,7 @@ pub struct Player {
 impl Player {
     fn new(_owner: &Spatial) -> Self {
         Player{
-          speed: 0.2
+          speed: 0.3
         }
     }
 
@@ -22,18 +22,28 @@ impl Player {
     }
 
     #[export]
-    fn _input(&self, owner: &Spatial, event: gdnative::Ref<InputEvent>) {
-      let event = unsafe { event.assume_safe() };
-      if event.is_action_pressed("move_forward", true) {
+    fn _physics_process(&self, owner: &Spatial, delta: f32) {
+      self._process_input(owner, delta);
+
+    }
+
+    #[export]
+    fn _process_input(&self, owner: &Spatial, delta: f32) {
+      let input = Input::godot_singleton();
+      let dir = Vector3::new(0.0,0.0,0.0);
+
+      let cam_xform = owner.find_node("Camera", true, true);
+
+      if input.is_action_pressed("move_forward") {
         self._move(owner, "forward".to_string())
       }
-      if event.is_action_pressed("move_backward", true) {
+      if input.is_action_pressed("move_backward") {
         self._move(owner, "backward".to_string())
       }
-      if event.is_action_pressed("move_left", true) {
+      if input.is_action_pressed("move_left") {
         self._move(owner, "left".to_string())
       }
-      if event.is_action_pressed("move_right", true) {
+      if input.is_action_pressed("move_right") {
         self._move(owner, "right".to_string())
       }
     }
@@ -42,10 +52,15 @@ impl Player {
     fn _move(&self, owner: &Spatial, direction: String) {
       match direction {
         _ if direction == "forward" => owner.translate(- owner.global_transform().basis.z() * self.speed),
-        _ if direction == "backward" => owner.translate(owner.global_transform().basis.z()),
-        _ if direction == "left" => owner.translate(- owner.global_transform().basis.x()),
-        _ if direction == "right" => owner.translate(owner.global_transform().basis.x()),
+        _ if direction == "backward" => owner.translate(owner.global_transform().basis.z() * self.speed),
+        _ if direction == "left" => owner.translate(- owner.global_transform().basis.x() * self.speed),
+        _ if direction == "right" => owner.translate(owner.global_transform().basis.x() * self.speed),
         _ => godot_print!("Doing nothing")
       }
+    }
+
+    #[export]
+    fn _get_player_representation(&self, owner: &Spatial) -> Option<Ref<Node>> {
+      owner.get_child(0)
     }
 }
