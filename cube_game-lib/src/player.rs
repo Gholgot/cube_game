8b90d6670803cded3568e698818b0852334a8907
Tony Jacomelli <tjacomelli@gmail.com>
2021-04-23 09:11:23 +0200

    Working on the player movement, next step use linear velocity and impulse instead of translate

--- cube_game-lib/src/player.rs
+++ cube_game-lib/src/player.rs
@@ -1,66 +1,53 @@
 use gdnative::prelude::*;
-use gdnative::api::Spatial;
+use gdnative::api::RigidBody;
 
 #[derive(NativeClass)]
-#[inherit(Spatial)]
+#[inherit(RigidBody)]
 pub struct Player {
-  speed: f32
+  speed: f32,
+  gravity: f32,
+  velocity: Vector3,
 }
 
 #[gdnative::methods]
 impl Player {
-    fn new(_owner: &Spatial) -> Self {
+    fn new(_owner: &RigidBody) -> Self {
         Player{
-          speed: 0.3
+          speed: 0.2,
+          gravity: -24.0,
+          velocity: Vector3::new(0.0,0.0,0.0)
         }
     }
 
     #[export]
-    fn _ready(&self, _owner: &Spatial) {
-        //! To remove
-        godot_print!("Player class initialized.")
+    fn _physics_process(&self, owner: &RigidBody, _delta: f32) {
+      //owner.translate(owner.global_transform().basis.y() * (_delta * self.gravity));
+      self._process_input(owner, _delta);
     }
 
     #[export]
-    fn _physics_process(&self, owner: &Spatial, delta: f32) {
-      self._process_input(owner, delta);
-
-    }
-
-    #[export]
-    fn _process_input(&self, owner: &Spatial, delta: f32) {
+    fn _process_input(&self, owner: &RigidBody, _delta: f32) {
       let input = Input::godot_singleton();
-      let dir = Vector3::new(0.0,0.0,0.0);
+      let _dir = Vector3::new(0.0,0.0,0.0);
 
-      let cam_xform = owner.find_node("Camera", true, true);
+      let _cam_xform = owner.find_node("Camera", true, true);
 
       if input.is_action_pressed("move_forward") {
-        self._move(owner, "forward".to_string())
+        owner.translate(- owner.global_transform().basis.z() * self.speed)
       }
       if input.is_action_pressed("move_backward") {
-        self._move(owner, "backward".to_string())
+        owner.translate(owner.global_transform().basis.z() * self.speed)
       }
       if input.is_action_pressed("move_left") {
-        self._move(owner, "left".to_string())
+        owner.translate(- owner.global_transform().basis.x() * self.speed)
       }
       if input.is_action_pressed("move_right") {
-        self._move(owner, "right".to_string())
+        owner.translate(owner.global_transform().basis.x() * self.speed)
       }
     }
 
     #[export]
-    fn _move(&self, owner: &Spatial, direction: String) {
-      match direction {
-        _ if direction == "forward" => owner.translate(- owner.global_transform().basis.z() * self.speed),
-        _ if direction == "backward" => owner.translate(owner.global_transform().basis.z() * self.speed),
-        _ if direction == "left" => owner.translate(- owner.global_transform().basis.x() * self.speed),
-        _ if direction == "right" => owner.translate(owner.global_transform().basis.x() * self.speed),
-        _ => godot_print!("Doing nothing")
-      }
-    }
-
-    #[export]
-    fn _get_player_representation(&self, owner: &Spatial) -> Option<Ref<Node>> {
+    fn _get_player_representation(&self, owner: &RigidBody) -> Option<Ref<Node>> {
       owner.get_child(0)
     }
 }
